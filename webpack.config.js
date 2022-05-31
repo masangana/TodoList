53588953df78dbd8e3551235c5597e142ea742b6
masangana <alexmasangana@gmail.com>
2022-06-01 01:19:53 +0300

    set_up webpack project

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+
+    static: './dist',
+
+  },
+
+  plugins: [
+
+    new HtmlWebpackPlugin({
+
+        template: './src/index.html',
+
+    }),
+
+  ],
+
+  module: {
+
+    rules: [
+
+      {
+
+        test: /\.css$/i,
+
+        use: ['style-loader', 'css-loader'],
+
+      },
+
+    ],
+
+  },
+
+  optimization: {
+
+    runtimeChunk: 'single',
+
+  },
+
+};
