f6a73f8f18e9478c567b2e2f45e5d3ad62b39a6a
masangana <alexmasangana@gmail.com>
2022-06-01 15:48:22 +0300

    set html template & css style

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import _ from 'lodash';
-import './style.css';
+import 'bootstrap';
+import './style.scss';
 
 function component() {
     const element = document.createElement('div');
   
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' '); 
-    element.classList.add('hello');
+   // element.innerHTML = _.join(['Hello', 'webpack'], ' '); 
+    //element.classList.add('hello');
   
     return element;
   }
